dbacd1aefe41b49bf38ea41733109dd2c908a6b2
TBye101 <ThomasBye1@gmail.com>
2020-12-20 22:53:28 -0600

    Initial project creation

---
+++ cliargs_t/target/rls/debug/deps/cliargs_t-14a6d9eb9ad8ed03.d
@@ -0,0 +1,5 @@
+/mnt/DATA/Repos/cliargs_t/cliargs_t/target/rls/debug/deps/cliargs_t-14a6d9eb9ad8ed03.rmeta: src/lib.rs
+
+/mnt/DATA/Repos/cliargs_t/cliargs_t/target/rls/debug/deps/cliargs_t-14a6d9eb9ad8ed03.d: src/lib.rs
+
+src/lib.rs:
---
+++ cliargs_t/target/rls/debug/deps/cliargs_t-bf2ea193a946c3ce.d
@@ -0,0 +1,5 @@
+/mnt/DATA/Repos/cliargs_t/cliargs_t/target/rls/debug/deps/cliargs_t-bf2ea193a946c3ce.rmeta: src/lib.rs
+
+/mnt/DATA/Repos/cliargs_t/cliargs_t/target/rls/debug/deps/cliargs_t-bf2ea193a946c3ce.d: src/lib.rs
+
+src/lib.rs:
